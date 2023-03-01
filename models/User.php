a6dfab995979599e7d91365d8ebc0609fa70848c
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-03-01 17:10:39 +0700

    Polish Signup and Login
    - Add signup link in navbar
    - Validate password with hash for login

--- models/User.php
+++ models/User.php
@@ -92,6 +92,6 @@ class User extends ActiveRecord implements \yii\web\IdentityInterface
      */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return \Yii::$app->security->validatePassword($password, $this->password);
     }
 }
