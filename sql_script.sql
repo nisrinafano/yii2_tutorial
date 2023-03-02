6294e3aa7333b5e066c580a264808d03b1f192ec
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-03-02 13:53:32 +0700

    Article initialization
    - Create article table
    - Generate article models
    - Generate CRUD for Article (include controller and views)

--- sql_script.sql
+++ sql_script.sql
@@ -14,3 +14,26 @@ ALTER TABLE `user`
 
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `title` varchar(1024) NOT NULL,
+  `slug` varchar(1024) NOT NULL,
+  `body` longtext NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `article_user_created_by_fk` (`created_by`),
+  ADD KEY `article_user_updated_by_fk` (`updated_by`);
+
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_user_created_by_fk` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `article_user_updated_by_fk` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`);
